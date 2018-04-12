d2b7414a83a29b6b722868f7cde0a7e7a7dcb2b4
Torben <mail@sauce-code.com>
2018-04-12 10:25:01 +0200

    set up pom and modules

---
+++ filtr-gui/src/main/java/com/saucecode/filtr/gui/MainWindow.java
@@ -0,0 +1,14 @@
+package com.saucecode.filtr.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
