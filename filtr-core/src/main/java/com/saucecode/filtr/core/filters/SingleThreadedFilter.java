1000ebae62f6355f877133f67b75b2e8aa903484
Torben <mail@sauce-code.com>
2018-04-18 15:33:19 +0200

    implement singlethreadedfilter

--- filtr-core/src/main/java/com/saucecode/filtr/core/filters/SingleThreadedFilter.java
+++ filtr-core/src/main/java/com/saucecode/filtr/core/filters/SingleThreadedFilter.java
@@ -2,19 +2,39 @@ package com.saucecode.filtr.core.filters;
 
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 
 public abstract class SingleThreadedFilter implements Filter {
+	
+	protected SimpleDoubleProperty progress;
 
 	@Override
 	public Image filter(Image image) {
-		// TODO Auto-generated method stub
-		return null;
+		progress.set(0.0);
+		PixelReader pr = image.getPixelReader();
+		WritableImage wi = new WritableImage((int) image.getWidth(),
+				(int) image.getHeight());
+		PixelWriter pw = wi.getPixelWriter();
+		for (int x = 1; x < image.getWidth() - 1; x++) {
+			if (Thread.currentThread().isInterrupted()) {
+				  return image;
+			}
+			progress.set((double) x / image.getWidth()); 
+			for (int y = 1; y < image.getHeight() - 1; y++) {
+				computePixel(x, y, pr, pw);
+			}
+		}
+		progress.set(1.0);
+		return wi;
 	}
 
 	@Override
 	public void setProgress(SimpleDoubleProperty progress) {
-		// TODO Auto-generated method stub
-
+		this.progress = progress;
 	}
+	
+	protected abstract void computePixel(int x, int y, PixelReader pr, PixelWriter pw);
 
 }
