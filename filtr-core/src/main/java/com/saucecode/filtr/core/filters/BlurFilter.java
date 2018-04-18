783a0b7950b8fd7afd71a5c224049a19a82e7c21
Torben <mail@sauce-code.com>
2018-04-18 14:59:43 +0200

    implement multithreaded blurfilter

--- filtr-core/src/main/java/com/saucecode/filtr/core/filters/BlurFilter.java
+++ filtr-core/src/main/java/com/saucecode/filtr/core/filters/BlurFilter.java
@@ -1,5 +1,7 @@
 package com.saucecode.filtr.core.filters;
 
+import java.util.concurrent.CountDownLatch;
+
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.image.Image;
 import javafx.scene.image.PixelReader;
@@ -8,9 +10,9 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 
 public class BlurFilter implements Filter {
-	
+
 	public static final String NAME = "Blur";
-	
+
 	private SimpleDoubleProperty progress;
 
 	@Override
@@ -25,37 +27,68 @@ public class BlurFilter implements Filter {
 		WritableImage wi = new WritableImage((int) image.getWidth(),
 				(int) image.getHeight());
 		PixelWriter pw = wi.getPixelWriter();
-		for (int x = 1; x < image.getWidth() - 1; x++) {
-			if (Thread.currentThread().isInterrupted()) {
-				  return image;
-			}
-			progress.set((double) x / image.getWidth()); 
-			for (int y = 1; y < image.getHeight() - 1; y++) {
-				double red = pr.getColor(x, y).getRed() * 0.2
-						+ pr.getColor(x - 1, y).getRed() * 0.2
-						+ pr.getColor(x, y - 1).getRed() * 0.2
-						+ pr.getColor(x + 1, y).getRed() * 0.2
-						+ pr.getColor(x, y + 1).getRed() * 0.2;
-				double green = pr.getColor(x, y).getGreen() * 0.2
-						+ pr.getColor(x - 1, y).getGreen() * 0.2
-						+ pr.getColor(x, y - 1).getGreen() * 0.2
-						+ pr.getColor(x + 1, y).getGreen() * 0.2
-						+ pr.getColor(x, y + 1).getGreen() * 0.2;
-				double blue = pr.getColor(x, y).getBlue() * 0.2
-						+ pr.getColor(x - 1, y).getBlue() * 0.2
-						+ pr.getColor(x, y - 1).getBlue() * 0.2
-						+ pr.getColor(x + 1, y).getBlue() * 0.2
-						+ pr.getColor(x, y + 1).getBlue() * 0.2;
-				double opacity = pr.getColor(x, y).getOpacity() * 0.2
-						+ pr.getColor(x - 1, y).getOpacity() * 0.2
-						+ pr.getColor(x, y - 1).getOpacity() * 0.2
-						+ pr.getColor(x + 1, y).getOpacity() * 0.2
-						+ pr.getColor(x, y + 1).getOpacity() * 0.2;
-				pw.setColor(x, y, new Color(red, green, blue, opacity));
-			}
+		if (Thread.currentThread().isInterrupted()) {
+			return image;
+		}
+		int processors = Runtime.getRuntime().availableProcessors() * 2;
+		final CountDownLatch latch = new CountDownLatch(processors);
+		for (int p = 0; p < processors; p++) {
+			final int current = (int) (image.getHeight() * p / processors);
+			final int next = (int) (image.getHeight() * (p + 1) / processors);
+			final int width = (int) image.getWidth();
+			final int height = (int) image.getHeight();
+			new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					System.out.println("Thread "
+							+ Thread.currentThread().getName() + " started");
+					for (int y = (current == 0 ? 1 : current); y < next
+							- 1; y++) {
+						for (int x = 1; x < width - 1; x++) {
+							doPixel(x, y, pr, pw);
+						}
+						progress.set(progress.doubleValue() + 1.0 / height);
+
+					}
+					latch.countDown();
+				}
+
+			}).start();
+
+		}
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		progress.set(1.0);
 		return wi;
+	}
+
+	private void doPixel(int x, int y, PixelReader pr, PixelWriter pw) {
+		double red = pr.getColor(x, y).getRed() * 0.2
+				+ pr.getColor(x - 1, y).getRed() * 0.2
+				+ pr.getColor(x, y - 1).getRed() * 0.2
+				+ pr.getColor(x + 1, y).getRed() * 0.2
+				+ pr.getColor(x, y + 1).getRed() * 0.2;
+		double green = pr.getColor(x, y).getGreen() * 0.2
+				+ pr.getColor(x - 1, y).getGreen() * 0.2
+				+ pr.getColor(x, y - 1).getGreen() * 0.2
+				+ pr.getColor(x + 1, y).getGreen() * 0.2
+				+ pr.getColor(x, y + 1).getGreen() * 0.2;
+		double blue = pr.getColor(x, y).getBlue() * 0.2
+				+ pr.getColor(x - 1, y).getBlue() * 0.2
+				+ pr.getColor(x, y - 1).getBlue() * 0.2
+				+ pr.getColor(x + 1, y).getBlue() * 0.2
+				+ pr.getColor(x, y + 1).getBlue() * 0.2;
+		double opacity = pr.getColor(x, y).getOpacity() * 0.2
+				+ pr.getColor(x - 1, y).getOpacity() * 0.2
+				+ pr.getColor(x, y - 1).getOpacity() * 0.2
+				+ pr.getColor(x + 1, y).getOpacity() * 0.2
+				+ pr.getColor(x, y + 1).getOpacity() * 0.2;
+		pw.setColor(x, y, new Color(red, green, blue, opacity));
 	}
 
 	@Override
